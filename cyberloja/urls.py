774782625e4b79f31d27b64e3e4f934abbd79f89
Lucas <lucascomaru20@gmail.com>
2023-04-03 13:07:25 -0300

    Bases HTML criadas e URLS ajustados

--- cyberloja/urls.py
+++ cyberloja/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('/', include('loja.urls'))
+    path('', include('loja.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
