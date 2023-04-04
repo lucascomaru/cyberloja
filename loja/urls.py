c73d26faeb3c3157b75f9163f9f4e418b179ab51
Lucas <lucascomaru20@gmail.com>
2023-04-04 13:45:44 -0300

    Construindo tela de Produtos e Carrinho

--- loja/urls.py
+++ loja/urls.py
@@ -1,10 +1,11 @@
 # url - #view - template
 
 from django.urls import path, include
-from.views import homepage, contato
+from.views import homepage, contato, lista_produtos, carrinho
 
 urlpatterns = [
     path('', homepage),
     path('contato', contato),
-
+    path('produtos/', lista_produtos, name='produtos'),
+    path('carrinho/', carrinho, name='carrinho'),
 ]
--- loja/views.py
+++ loja/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-
+from .models import Produto, Carrinho
 # Create your views here.
 def homepage(request):
     return render(request, "homepage.html")
 
 def contato(request):
     return render(request, "contato.html")
+
+def lista_produtos(request):
+    produtos = Produto.objects.all()
+    return render(request, 'produtos.html', {'produtos': produtos})
+
+def carrinho(request):
+    itens_do_carrinho = Carrinho.objects.filter(usuario=request.user)
+    context = {'itens_do_carrinho': itens_do_carrinho}
+    return render(request, 'carrinho.html', context)
